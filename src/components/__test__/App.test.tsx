cff35ea2247f7c1326231f8bee44bf35bc48152a
Eduard Ptushko <ptushko@gmail.com>
2019-10-20 15:49:24 +0300

    add src/types and Open Sans fonts

--- src/components/__test__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import App from "../App";
-
-test("should render correctly", (): void => {
-    const wrapper = shallow(<App />);
-
-    expect(wrapper).toMatchSnapshot();
-});
